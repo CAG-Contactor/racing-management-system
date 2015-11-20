d9930a5130c5a7b959df8f575f383777dbfe626c
Daniel Wikman <daniel.wikman@cag.se>
2015-11-20 13:25:25 +0100

    First implementation.

---
+++ user-manager/src/main/java/se/cag/labs/usermanager/Token.java
@@ -0,0 +1,24 @@
+package se.cag.labs.usermanager;
+
+/**
+ * Created by dawi on 2015-11-20.
+ */
+public class Token {
+    private String token;
+
+    public Token() {
+
+    }
+
+    public Token(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
