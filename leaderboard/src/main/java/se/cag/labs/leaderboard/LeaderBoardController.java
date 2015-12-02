564844867696359694488bfc10b6587fa5bc5da6
Joacim Breiler <joacim@nimlab.se>
2015-12-02 19:41:24 +0100

    Lade till lite mer Swagger-docs

--- leaderboard/src/main/java/se/cag/labs/leaderboard/LeaderBoardController.java
+++ leaderboard/src/main/java/se/cag/labs/leaderboard/LeaderBoardController.java
@@ -1,6 +1,10 @@
 package se.cag.labs.leaderboard;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(basePath = "*", value = "Leaderboard", description = "Leaderboard for all races", produces = "application/json")
 @RestController
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class LeaderBoardController {
@@ -17,13 +22,27 @@ public class LeaderBoardController {
     private LeaderBoardRepository repository;
 
     @RequestMapping(value = "/results", method = RequestMethod.POST)
+    @ApiOperation(
+            value = "Saves a new result to the leaderboard",
+            notes = "Saves a new result to the leaderboard.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "The results where saved successfully"),
+    })
     public void newResult(@RequestBody UserResult userResult) {
         repository.insert(userResult);
     }
 
     @RequestMapping(value="/results", method = RequestMethod.GET)
-    public UserResult[] results() {
-        List<UserResult> all = repository.findAll();
-        return all.toArray(new UserResult[all.size()]);
+    @ApiOperation(
+            value = "Returns the leaderboard",
+            notes = "This will return the leaderboard sorted on fastest result time first. All results that doesn't have the result status FINISHED are sorted last.",
+            response = UserResult.class,
+            responseContainer = "List")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "The leaderboard is returned"),
+    })
+    public List<UserResult> results() {
+        // TODO sort on time
+        return repository.findAll();
     }
 }
