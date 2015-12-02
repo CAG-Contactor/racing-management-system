f6be962229a3c7362426b68fb948f9f7179afb8f
oGre <ogre@muppfarmen.se>
2015-12-02 20:42:46 +0100

    Fixade testfallen som använder callback-URL. Kräver att raceadministrator är igång för att testfallen ska fungera. Ändrade så att vi postar pårt api-objekt och inte det råa objektet från mongo

--- current-race/src/main/java/se/cag/labs/currentrace/services/CallbackService.java
+++ current-race/src/main/java/se/cag/labs/currentrace/services/CallbackService.java
@@ -8,6 +8,7 @@ package se.cag.labs.currentrace.services;
 import lombok.extern.java.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.client.*;
+import se.cag.labs.currentrace.apicontroller.mapper.ModelMapper;
 import se.cag.labs.currentrace.services.repository.datamodel.*;
 
 @Component
@@ -16,6 +17,6 @@ public class CallbackService {
     private RestTemplate restTemplate = new RestTemplate();
 
     public void reportStatus(RaceStatus status) {
-        restTemplate.postForLocation(status.getCallbackUrl(), status);
+        restTemplate.postForLocation(status.getCallbackUrl(), ModelMapper.createStatusResponse(status));
     }
 }
