9be2d332d5a3b8efeac2efea74d37a9654fc7eac
Joel Binnquist <joel.binnquist@gmail.com>
2015-12-06 19:59:09 +0100

    Implementera test med mockad resttemplate i CallBackService.

--- current-race/src/main/java/se/cag/labs/currentrace/apicontroller/apimodel/RaceStatus.java
+++ current-race/src/main/java/se/cag/labs/currentrace/apicontroller/apimodel/RaceStatus.java
@@ -4,13 +4,15 @@ package se.cag.labs.currentrace.apicontroller.apimodel;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import lombok.Builder;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.Date;
 
 @Builder
+@AllArgsConstructor
+@Getter
 @ToString
+@EqualsAndHashCode
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonDeserialize(builder = RaceStatus.RaceStatusBuilder.class)
 public class RaceStatus {
--- current-race/src/main/java/se/cag/labs/currentrace/services/CallbackService.java
+++ current-race/src/main/java/se/cag/labs/currentrace/services/CallbackService.java
@@ -1,12 +1,14 @@
 package se.cag.labs.currentrace.services;
 
 import lombok.extern.log4j.*;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.client.*;
 import se.cag.labs.currentrace.apicontroller.mapper.*;
 import se.cag.labs.currentrace.services.repository.datamodel.*;
 
 @Component
+@Scope("singleton")
 @Log4j
 public class CallbackService {
     private RestTemplate restTemplate = new RestTemplate();
