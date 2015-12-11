382a5666a991efa6f7f5058ef40e1871f4ecb9fc
Ove Noros <ove.noros@tillvaxtverket.se>
2015-12-11 15:17:09 +0100

    Minor

--- cag-rms-client/src/main/webapp_src/src/app/leaderboard/leaderboard.directive.js
+++ cag-rms-client/src/main/webapp_src/src/app/leaderboard/leaderboard.directive.js
@@ -1,18 +1,33 @@
 "use strict";
-angular.module('cag-rms-client').directive('leaderboard', function ($log, $http) {
-  $log.log("in leaderboard");
-  $http({
-    method: 'GET',
-    url: 'http://localhost:10180/results'
-  }).then(function successCallback(response) {
-    console.debug(response.data);
-  }, function errorCallback(response) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-  });
+(function () {
+  angular.module('cag-rms-client').directive('leaderboard', factory);
 
-  return {
-    restrict: 'E',
-    templateUrl: 'leaderboard/leaderboard.tpl.html'
-  };
-});
+  function factory($log, $http) {
+    $log.log("in leaderboard");
+
+    $http({
+      method: 'GET',
+      url: 'http://localhost:10180/results'
+    }).then(function successCallback(response) {
+      console.debug(response.data);
+    }, function errorCallback(response) {
+    });
+
+    return {
+      restrict: 'E',
+      templateUrl: 'leaderboard/leaderboard.tpl.html',
+      controller: Ctrl,
+      controllerAs: 'vm'
+    };
+  }
+
+  function Ctrl(localStorageService) {
+    var vm = this;
+    vm.results = results;
+
+    function results() {
+      console.debug('Results:');
+      return 'Kalle';
+    }
+  }
+}());
