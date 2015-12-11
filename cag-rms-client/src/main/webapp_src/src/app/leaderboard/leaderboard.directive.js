1a759262526b85e4128b83c4318d4f39ced6b75d
Ove Noros <ove.noros@tillvaxtverket.se>
2015-12-11 14:59:40 +0100

    Minor

--- cag-rms-client/src/main/webapp_src/src/app/leaderboard/leaderboard.directive.js
+++ cag-rms-client/src/main/webapp_src/src/app/leaderboard/leaderboard.directive.js
@@ -1,6 +1,16 @@
 "use strict";
-angular.module('cag-rms-client').directive('leaderboard', function ($log) {
+angular.module('cag-rms-client').directive('leaderboard', function ($log, $http) {
   $log.log("in leaderboard");
+  $http({
+    method: 'GET',
+    url: 'http://localhost:10180/results'
+  }).then(function successCallback(response) {
+    console.debug(response.data);
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+
   return {
     restrict: 'E',
     templateUrl: 'leaderboard/leaderboard.tpl.html'
