e98a82a5415805bbd30ec80017e166ecd0124d1c
oGre <ogre@muppfarmen.se>
2015-12-11 22:19:41 +0100

    Provar att läsa direkt från current race status och kör bara timern om lopp pågår

--- cag-rms-client/src/main/webapp_src/src/app/currentrace/currentrace.directive.js
+++ cag-rms-client/src/main/webapp_src/src/app/currentrace/currentrace.directive.js
@@ -1,18 +1,42 @@
 'use strict';
 (function () {
-    angular.module('cag-rms-client').directive('currentRace', ['$timeout', function ($timeout) {
+    angular.module('cag-rms-client').directive('currentRace', ['$timeout', '$http', function ($timeout, $http) {
         return {
             scope: {},
             restrict: 'E',
             link: function (scope, elems, attrs) {
-                scope.currentUser = {name: 'Nisse', startTime: Date.now(), splitTime: new Date(), finishTime: new Date()};
-                scope.startTimeInMillis = +scope.currentUser.startTime;
+                $http({
+                    method: 'GET',
+                    url: 'http://localhost:10080/status'
+                }).then(function successCallback(response) {
+                    console.debug(response.data);
+                    scope.isActive = response.data.state === 'ACTIVE';
+                    if (response.data.startTime) {
+                        scope.startTime = new Date(response.data.startTime);
+                    }
+                    else {
+                        scope.startTime = new Date();
+                    }
+                    scope.startTimeInMillis = +scope.startTime;
+
+                    if (response.data.middleTime) {
+                        scope.splitTime = new Date(response.data.middleTime);
+                    }
+                    if (response.data.finishTime) {
+                        scope.finishTime = new Date(response.data.finishTime);
+                    }
+                }, function errorCallback(response) {
+                });
+                //scope.currentUser = {name: 'Nisse', startTime: Date.now(), splitTime: new Date(), finishTime: new Date()};
+                //scope.startTimeInMillis = +scope.currentUser.startTime;
                 console.log("time started");
                 timer();
 
                 function timer() {
-                    scope.currentUser.startTime = Date.now() - scope.startTimeInMillis;
-                    $timeout(timer, 100);
+                    if (scope.isActive) {
+                        scope.startTime = Date.now() - scope.startTimeInMillis;
+                        $timeout(timer, 100);
+                    }
                 }
             },
             templateUrl: 'currentrace/currentrace.tpl.html'
