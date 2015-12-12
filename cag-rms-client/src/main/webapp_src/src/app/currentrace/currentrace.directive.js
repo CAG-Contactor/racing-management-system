c1f36bd37500e656de7fe61aa4895904bee7c6c8
FredrikDahlman <fredrik.dahlman@gmail.com>
2015-12-12 09:00:59 +0100

    Put the get status HTTP call in a "self called" function to get updates from back-end to gui.

--- cag-rms-client/src/main/webapp_src/src/app/currentrace/currentrace.directive.js
+++ cag-rms-client/src/main/webapp_src/src/app/currentrace/currentrace.directive.js
@@ -5,41 +5,51 @@
             scope: {},
             restrict: 'E',
             link: function (scope, elems, attrs) {
-                $http({
-                    method: 'GET',
-                    url: 'http://localhost:10080/status'
-                }).then(function successCallback(response) {
-                    console.debug(response.data);
-                    scope.isActive = response.data.state === 'ACTIVE';
-                    if (response.data.startTime) {
-                        scope.startTime = new Date(response.data.startTime);
-                    }
-                    else {
-                        scope.startTime = new Date();
-                    }
-                    scope.startTimeInMillis = +scope.startTime;
-
-                    if (response.data.middleTime) {
-                        scope.splitTime = new Date(response.data.middleTime);
-                    }
-                    if (response.data.finishTime) {
-                        scope.finishTime = new Date(response.data.finishTime);
-                    }
-                }, function errorCallback(response) {
-                });
-                //scope.currentUser = {name: 'Nisse', startTime: Date.now(), splitTime: new Date(), finishTime: new Date()};
-                //scope.startTimeInMillis = +scope.currentUser.startTime;
-                console.log("time started");
+                scope.isActive = false;
+                scope.startTime = 0;
+                getStatus();
                 timer();
 
                 function timer() {
                     if (scope.isActive) {
                         scope.startTime = Date.now() - scope.startTimeInMillis;
-                        $timeout(timer, 100);
                     }
+                    $timeout(timer, 100);
+                }
+
+                function getStatus() {
+                    $http({
+                        method: 'GET',
+                        url: 'http://localhost:10080/status'
+                    }).then(function successCallback(response) {
+                        console.debug(response.data);
+
+                        var newActiveState = response.data.state === 'ACTIVE';
+                        if(scope.isActive != newActiveState) {
+                            console.debug('isActive changed to ' + newActiveState);
+                            scope.isActive = newActiveState;
+                            if (response.data.startTime) {
+                                scope.startTime = new Date(response.data.startTime);
+                            }
+                            else {
+                                scope.startTime = new Date();
+                            }
+                            scope.startTimeInMillis = +scope.startTime;
+
+                            if (response.data.middleTime) {
+                                scope.splitTime = new Date(response.data.middleTime);
+                            }
+                            if (response.data.finishTime) {
+                                scope.finishTime = new Date(response.data.finishTime);
+                            }
+                        }
+                    }, function errorCallback(response) {
+                    });
+                    $timeout(getStatus, 1000);
                 }
             },
             templateUrl: 'currentrace/currentrace.tpl.html'
         };
+
     }]);
 }());
