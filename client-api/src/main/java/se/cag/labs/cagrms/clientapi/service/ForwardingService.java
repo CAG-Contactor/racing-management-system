9e23ee9ab750ab81c51f16b39f3decb99b346f5c
Lbg74 <lars.borg@cag.se>
2016-01-19 22:01:48 +0100

    Kopplat ihop client-api med race status och user queue

--- client-api/src/main/java/se/cag/labs/cagrms/clientapi/service/ForwardingService.java
+++ client-api/src/main/java/se/cag/labs/cagrms/clientapi/service/ForwardingService.java
@@ -20,6 +20,10 @@ public class ForwardingService {
     private String userManagerBaseUri;
     @Value("${server.leaderboard.base.uri}")
     private String leaderBoardBaseUri;
+    @Value("${server.currentrace.base.uri}")
+    private String currentRaceBaseUri;
+    @Value("${server.raceadmin.base.uri}")
+    private String raceAdminBaseUri;
 
     private RestTemplate restTemplate = new RestTemplate();
 
@@ -57,11 +61,37 @@ public class ForwardingService {
 
     public ResponseEntity<List<UserResult>> getResults() {
         final URI uri = UriComponentsBuilder
-            .fromHttpUrl(leaderBoardBaseUri + "/results")
-            .build()
-            .toUri();
+                .fromHttpUrl(leaderBoardBaseUri + "/results")
+                .build()
+                .toUri();
         try {
             final ResponseEntity<List<UserResult>> response = restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<List<UserResult>>() {});
+            return response;
+        } catch (HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(null);
+        }
+    }
+
+    public ResponseEntity<List<User>> getQueue() {
+        final URI uri = UriComponentsBuilder
+                .fromHttpUrl(raceAdminBaseUri + "/userqueue")
+                .build()
+                .toUri();
+        try {
+            final ResponseEntity<List<User>> response = restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>() {});
+            return response;
+        } catch (HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(null);
+        }
+    }
+
+    public ResponseEntity<RaceStatus> getStatus() {
+        final URI uri = UriComponentsBuilder
+                .fromHttpUrl(currentRaceBaseUri + "/status")
+                .build()
+                .toUri();
+        try {
+            final ResponseEntity<RaceStatus> response = restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<RaceStatus>() {});
             return response;
         } catch (HttpStatusCodeException e) {
             return ResponseEntity.status(e.getStatusCode()).body(null);
