fbc4970d9a2abd3b4b321c5957eea7fdaa0555e3
nilden <stefan.nilden@gmail.com>
2016-01-27 18:22:10 +0100

    Snyggar till lite allmänt i koden

--- pi-sensor/src/main/java/se/cag/labs/pisensor/TimerSensor.java
+++ pi-sensor/src/main/java/se/cag/labs/pisensor/TimerSensor.java
@@ -1,16 +1,12 @@
 package se.cag.labs.pisensor;
 
 import com.pi4j.io.gpio.*;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 
 /**
@@ -21,9 +17,6 @@ public class TimerSensor {
     private static long startTime;
     private static long splitTime;
     private static long finishTime;
-
-    @Value("${server.raceadministrator.base.uri}")
-    private static String baseUri;
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("GPIO TimerSensor ... started.");
@@ -36,37 +29,28 @@ public class TimerSensor {
 
         System.out.println(" ... PRESS <CTRL-C> TO STOP THE PROGRAM.");
 
-        startSensor.addListener(new GpioPinListenerDigital() {
-            @Override
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                if (event.getState() == PinState.HIGH) {
-                    startTime = System.currentTimeMillis();
+        startSensor.addListener((GpioPinListenerDigital) event -> {
+            if (event.getState() == PinState.HIGH) {
+                startTime = System.currentTimeMillis();
 
-                    System.out.println(" --> CHANGE ON START SENSOR - TIME: 0" + startTime);
-                    registerEvent("START", String.valueOf(startTime));
-                }
+                System.out.println(" --> CHANGE ON START SENSOR - TIME: 0" + startTime);
+                registerEvent("START", String.valueOf(startTime));
             }
         });
 
-        splitSensor.addListener(new GpioPinListenerDigital() {
-            @Override
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                if (event.getState() == PinState.HIGH) {
-                    splitTime = System.currentTimeMillis();
-                    System.out.println(" --> CHANGE ON SPLIT SENSOR - TIME: " + splitTime);
-                    registerEvent("SPLIT", String.valueOf(splitTime));
-                }
+        splitSensor.addListener((GpioPinListenerDigital) event -> {
+            if (event.getState() == PinState.HIGH) {
+                splitTime = System.currentTimeMillis();
+                System.out.println(" --> CHANGE ON SPLIT SENSOR - TIME: " + splitTime);
+                registerEvent("SPLIT", String.valueOf(splitTime));
             }
         });
 
-        finishSensor.addListener(new GpioPinListenerDigital() {
-            @Override
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                if (event.getState() == PinState.HIGH) {
-                    finishTime = System.currentTimeMillis();
-                    System.out.println(" --> CHANGE ON FINISH SENSOR - TIME: " + finishTime);
-                    registerEvent("FINISH", String.valueOf(finishTime));
-                }
+        finishSensor.addListener((GpioPinListenerDigital) event -> {
+            if (event.getState() == PinState.HIGH) {
+                finishTime = System.currentTimeMillis();
+                System.out.println(" --> CHANGE ON FINISH SENSOR - TIME: " + finishTime);
+                registerEvent("FINISH", String.valueOf(finishTime));
             }
         });
 
@@ -93,10 +77,6 @@ public class TimerSensor {
 
             conn.disconnect();
 
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
