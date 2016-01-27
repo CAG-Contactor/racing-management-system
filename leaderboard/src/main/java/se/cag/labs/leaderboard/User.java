ecd33582ce240f22d7680f688a75667f55fc7953
Joacim Breiler <joacim@nimlab.se>
2016-01-27 20:12:27 +0100

    Duplicerat User i Leaderboard för att undvika referera till UserManager (får ClassNotFound när jag kör servern)

---
+++ leaderboard/src/main/java/se/cag/labs/leaderboard/User.java
@@ -0,0 +1,22 @@
+package se.cag.labs.leaderboard;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class User{
+
+  @Id
+  @JsonIgnore
+  private String id;
+  private Long timestamp;
+  private String userId;
+  private String displayName;
+}
