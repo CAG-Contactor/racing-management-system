54d4c0e8a02d16572c4d10342d0d9a59aa1414d3
oGre <ogre@muppfarmen.se>
2016-02-02 20:11:23 +0100

    Flyttade in protokollet för clientApi i variabeln så att vi kan ha https i produktion

--- cag-rms-client/src/main/webapp_src/src/app/config.js
+++ cag-rms-client/src/main/webapp_src/src/app/config.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
   angular.module('cag-rms-client').constant('APP_CONFIG', {
-    clientApi: 'localhost:10580' //Replaced by grunt copy
+    clientApi: 'http://localhost:10580' //Replaced by grunt copy
   });
 }());
