27bb61826288b0912a0695d8a7479e36546d111e
oGre <ogre@muppfarmen.se>
2016-02-06 18:04:20 +0100

    Provar att trigga cancelRace vid applikationsuppstart för att döda gammalt slagg i state-databasen

--- current-race/src/main/java/se/cag/labs/currentrace/CurrentRaceApplication.java
+++ current-race/src/main/java/se/cag/labs/currentrace/CurrentRaceApplication.java
@@ -1,13 +1,19 @@
 package se.cag.labs.currentrace;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.mongodb.repository.config.*;
+import se.cag.labs.currentrace.services.CancelRaceService;
 
 @SpringBootApplication
 @EnableMongoRepositories()
+@Log4j
 public class CurrentRaceApplication {
   public static void main(String[] args) {
-    SpringApplication.run(CurrentRaceApplication.class, args);
+    ConfigurableApplicationContext app = SpringApplication.run(CurrentRaceApplication.class, args);
+    CancelRaceService cancelRaceService = (CancelRaceService) app.getBean("cancelRaceService");
+    cancelRaceService.cancelRace();
   }
 }
