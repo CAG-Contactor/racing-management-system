c9f9adc8bf0e0d76214948aa61505e872973259e
Joel Binnquist <joel.binnquist@gmail.com>
2016-12-02 15:43:04 +0100

    Byt till JUnit4 i admin-backend.

---
+++ admin-backend/src/test/java/se/cag/labs/cagrms/admin/AdminApplicationTest.java
@@ -0,0 +1,19 @@
+package se.cag.labs.cagrms.admin;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+public class AdminApplicationTest {
+  @Rule
+  public MockitoRule rule = MockitoJUnit.rule();
+  @InjectMocks
+  private AdminApplication adminApplication;
+
+  @Test
+  public void it_should_run() throws Exception {
+    adminApplication.run(null, null);
+  }
+}
