f3502fdc58f403ae0ed8453853e92593cfa1d605
Joel Binnquist <joel.binnquist@gmail.com>
2016-12-02 15:17:28 +0100

    Lägg till admin-backend

---
+++ admin-backend/src/main/java/se/cag/labs/cagrms/admin/AdminConfiguration.java
@@ -0,0 +1,9 @@
+package se.cag.labs.cagrms.admin;
+
+import io.dropwizard.Configuration;
+
+/**
+ * Configuration for the admin client.
+ */
+public class AdminConfiguration extends Configuration {
+}
