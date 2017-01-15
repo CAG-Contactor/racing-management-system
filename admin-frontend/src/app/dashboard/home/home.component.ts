2ca8969a56d1781c12b593a1efc614109fa9313c
Joel Binnquist <joel.binnquist@gmail.com>
2017-01-15 16:10:01 +0100

    Admin WIP

--- admin-frontend/src/app/dashboard/home/home.component.ts
+++ admin-frontend/src/app/dashboard/home/home.component.ts
@@ -1,17 +1,5 @@
 import {Component} from "@angular/core";
 
-/**
- *  This class represents the lazy loaded HomeComponent.
- */
-
-@Component({
-  selector: 'timeline-cmp',
-  templateUrl: './timeline.html',
-  styleUrls: ['./timeline.scss'],
-})
-export class TimelineComponent {
-}
-
 @Component({
   selector: 'notifications-cmp',
   templateUrl: './notifications.html'
@@ -25,51 +13,7 @@ export class NotificationComponent {
 })
 
 export class HomeComponent {
-  /* Carousel Variable */
-  myInterval: number = 5000;
-  index: number = 0;
-  slides: Array<any> = [];
-  imgUrl: Array<any> = [
-    `assets/image/slider1.jpg`,
-    `assets/image/slider2.jpg`,
-    `assets/image/slider3.jpg`,
-    `assets/image/slider0.jpg`
-  ];
-  /* END */
-  /* Alert component */
-  public alerts: Array<Object> = [
-    {
-      type: 'danger',
-      msg: 'Oh snap! Change a few things up and try submitting again.'
-    },
-    {
-      type: 'success',
-      msg: 'Well done! You successfully read this important alert message.',
-      closable: true
-    }
-  ];
-
-  public closeAlert(i: number): void {
-    this.alerts.splice(i, 1);
-  }
-
-  /* END*/
-
   constructor() {
-    for (let i = 0; i < 4; i++) {
-      this.addSlide();
-    }
   }
-
-  /* Carousel */
-  addSlide() {
-    let i = this.slides.length;
-    this.slides.push({
-      image: this.imgUrl[i],
-      text: `${['Dummy ', 'Dummy ', 'Dummy ', 'Dummy '][this.slides.length % 4]}
-      			${['text 0', 'text 1', 'text 2', 'text 3'][this.slides.length % 4]}`
-    });
-  }
-
   /* END */
 }
--- admin-frontend/src/app/dashboard/home/home.module.ts
+++ admin-frontend/src/app/dashboard/home/home.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {HomeComponent, TimelineComponent, NotificationComponent} from "./home.component";
+import {HomeComponent, NotificationComponent} from "./home.component";
 import {CarouselModule, DropdownModule, AlertModule} from "ng2-bootstrap";
 
 @NgModule({
   imports: [CommonModule, CarouselModule, DropdownModule, AlertModule],
-  declarations: [HomeComponent, TimelineComponent, NotificationComponent],
-  exports: [HomeComponent, TimelineComponent, NotificationComponent]
+  declarations: [HomeComponent, NotificationComponent],
+  exports: [HomeComponent, NotificationComponent]
 })
 
 export class HomeModule {
