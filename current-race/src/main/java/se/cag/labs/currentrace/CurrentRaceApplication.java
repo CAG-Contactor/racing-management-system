c298a69f72ccb84611de3df8339b2c2565bb1ca8
Joacim Breiler <joacim@nimlab.se>
2017-02-04 16:29:09 +0100

    Rensar gamla registrerade sensorer om de inte har uppdaterats på en timme

--- current-race/src/main/java/se/cag/labs/currentrace/CurrentRaceApplication.java
+++ current-race/src/main/java/se/cag/labs/currentrace/CurrentRaceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import se.cag.labs.currentrace.services.CancelRaceService;
 
 @SpringBootApplication
-@EnableMongoRepositories()
+@EnableMongoRepositories
+@EnableScheduling
 @Log4j
 public class CurrentRaceApplication {
   public static void main(String[] args) {
