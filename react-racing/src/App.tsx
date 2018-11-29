027599cf01a88ced7a2e6d342d0e4487c802628e
Joseph Ronakali <joseph@ronakali.com>
2018-11-29 18:50:11 +0100

    Add MyRaces component

--- react-racing/src/App.tsx
+++ react-racing/src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import './App.css';
 
 import logo from './logo.svg';
+import { MyRaces } from "./myraces/MyRaces";
 
 class App extends React.Component {
   public render() {
@@ -14,6 +15,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <MyRaces/>
       </div>
     );
   }
