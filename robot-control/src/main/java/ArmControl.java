8eeccde668a9450a56a5f19d3dcb7323165860d6
Anders Englesson <anders.englesson@cag.se>
2018-12-10 21:12:05 +0100

    Första försök med begränsningsvinklar.

--- robot-control/src/main/java/ArmControl.java
+++ robot-control/src/main/java/ArmControl.java
@@ -3,34 +3,42 @@ import processing.core.PVector;
 import java.util.ArrayList;
 
 public class ArmControl {
-    ArmSegment armSegmentFirst;
-    ArmSegment armSegmentSecond;
-    ArmSegment armSegmentThird;
+    private ArmSegment armSegmentFirst;
+    private ArmSegment armSegmentSecond;
+    private ArmSegment armSegmentThird;
+    private PVector rotationPlane;
+
 
     public ArmControl() {
-        armSegmentFirst = new ArmSegment(0F,68F,124F);
-        armSegmentSecond = new ArmSegment(armSegmentFirst, 124F);
-        armSegmentThird = new ArmSegment(armSegmentSecond, 192F);
+        armSegmentFirst = new ArmSegment(0F,68F,124F,165,15,0,0);
+        armSegmentSecond = new ArmSegment(armSegmentFirst, 124F,90,0,0,0);
+        armSegmentThird = new ArmSegment(armSegmentSecond, 192F,70,0,0,0);
+        System.out.println("start! 1:" + armSegmentFirst.getStartPoint() + " : " + armSegmentFirst.getEndPoint());
+        System.out.println("start! 2:" + armSegmentSecond.getStartPoint() + " : " + armSegmentSecond.getEndPoint());
+        System.out.println("start! 3:" + armSegmentThird.getStartPoint() + " : " + armSegmentThird.getEndPoint());
+        System.out.println("start! first:" + armSegmentFirst.getAbsoluteAngleInDeg() + " second: " + armSegmentSecond.getAbsoluteAngleInDeg() + " third: " + armSegmentThird.getAbsoluteAngleInDeg());
     }
 
     public ArrayList<Float> getNewJointAngles(Float X, Float Y, Float Z) {
         PVector newTarget = new PVector(X, Y);
-        for (int i=0; i<10; i++) {
+        for (int i=0; i<50; i++) {
+            System.out.println("runda " + i);
             armSegmentThird.backwardCalculation(newTarget);
             armSegmentSecond.backwardCalculation(armSegmentThird.getStartPoint());
             armSegmentFirst.backwardCalculation(armSegmentSecond.getStartPoint());
             armSegmentFirst.forwardCalculation(new PVector(0,68F));
             armSegmentSecond.forwardCalculation(armSegmentFirst.getEndPoint());
             armSegmentThird.forwardCalculation(armSegmentSecond.getEndPoint());
+            System.out.println("diff to target: " + armSegmentThird.calculateDiffToTarget(newTarget));
             if(armSegmentThird.calculateDiffToTarget(newTarget) < 0.2F) {
                 break;
             }
         }
 
         ArrayList<Float> newAngles = new ArrayList();
-        newAngles.add(armSegmentFirst.getAngleInDeg());
-        newAngles.add(armSegmentSecond.getAngleInDeg());
-        newAngles.add(armSegmentThird.getAngleInDeg());
+        newAngles.add(armSegmentFirst.getRelativeAngleInDeg());
+        newAngles.add(armSegmentSecond.getRelativeAngleInDeg());
+        newAngles.add(armSegmentThird.getRelativeAngleInDeg());
 
         return newAngles;
     }
--- robot-control/src/main/java/ArmSegment.java
+++ robot-control/src/main/java/ArmSegment.java
@@ -2,6 +2,7 @@ import processing.core.PVector;
 
 import static java.lang.StrictMath.cos;
 import static java.lang.StrictMath.sin;
+import static java.lang.StrictMath.tan;
 
 
 public class ArmSegment {
@@ -13,23 +14,52 @@ public class ArmSegment {
     private ArmSegment parent;
     private float absoluteAngle = (float)3.14/4;
     private float relativeAngle;
+    private Float constraintXtoYUp;
+    private Float constraintXtoYDown;
+    private Float constraintYtoZLeft;
+    private Float constraintYtoZRight;
 
-    protected ArmSegment(float x, float y, float length){
+    protected ArmSegment(float x, float y, float length, float constraintXYUpInDeg, float constraintXYDownInDeg, float constraintYZLeftInDeg, float constraintYZRightInDeg){
         startPoint = new PVector( x,y);
 
         segmentLength = length;
         calculateEndPoint();
         relativeAngle = absoluteAngle;
+        constraintXtoYUp = (float)Math.toRadians(constraintXYUpInDeg);
+        constraintXtoYDown = (float)Math.toRadians(constraintXYDownInDeg);
+        constraintYtoZLeft = (float)Math.toRadians(constraintYZLeftInDeg);
+        constraintYtoZRight = (float)Math.toRadians(constraintYZRightInDeg);
+        System.out.println("constraint angles up: " + constraintXYUpInDeg  + " down: " + constraintXYDownInDeg);
+//        constraintXtoYUp = (float)Math.toDegrees(constraintXYUpInDeg);
+//        constraintXtoYDown = (float)Math.toDegrees(constraintXYDownInDeg);
+//        constraintYtoZLeft = (float)Math.toDegrees(constraintYZLeftInDeg);
+//        constraintYtoZRight = (float)Math.toDegrees(constraintYZRightInDeg);
 
     }
 
-    protected ArmSegment(ArmSegment parentSegment, Float length) {
+    protected ArmSegment(ArmSegment parentSegment, Float length, float constraintXYUpInDeg, float constraintXYDownInDeg, float constraintYZLeftInDeg, float constraintYZRightInDeg) {
         parent = parentSegment;
         startPoint = new PVector(parent.endPoint.x, parent.endPoint.y) ;
         segmentLength = length;
         calculateEndPoint();
         relativeAngle = parentSegment.absoluteAngle - absoluteAngle;
+        constraintXtoYUp = (float)Math.toRadians(constraintXYUpInDeg);
+        constraintXtoYDown = (float)Math.toRadians(constraintXYDownInDeg);
+        constraintYtoZLeft = (float)Math.toRadians(constraintYZLeftInDeg);
+        constraintYtoZRight = (float)Math.toRadians(constraintYZRightInDeg);
+        System.out.println("constraint angles up: " + constraintXYUpInDeg  + " down: " + constraintXYDownInDeg);
+//        constraintXtoYUp = (float)Math.toDegrees(constraintXYUpInDeg);
+//        constraintXtoYDown = (float)Math.toDegrees(constraintXYDownInDeg);
+//        constraintYtoZLeft = (float)Math.toDegrees(constraintYZLeftInDeg);
+//        constraintYtoZRight = (float)Math.toDegrees(constraintYZRightInDeg);
 
+    }
+
+    private void setupConstraints() {
+        Float lengthXYup = segmentLength * (float)tan(constraintXtoYUp);
+        Float lengthXYdown = segmentLength * (float)tan(constraintXtoYDown);
+        Float lengthYZLeft = segmentLength * (float)tan(constraintYtoZLeft);
+        Float lengthYZRight = segmentLength * (float)tan(constraintYtoZRight);
     }
 
     protected void calculateEndPoint() {
@@ -55,11 +85,13 @@ public class ArmSegment {
         return (float)Math.toDegrees(absoluteAngle);
     }
 
+    protected Float getAbsoluteAngle() {return absoluteAngle;}
+
     protected Float getAngleInRad() {
         return relativeAngle;
     }
 
-    protected Float getAngleInDeg() {
+    protected Float getRelativeAngleInDeg() {
         return (float)Math.toDegrees(relativeAngle);
     }
 
@@ -69,6 +101,9 @@ public class ArmSegment {
         absoluteAngle = newDirectionVector.heading();
         newDirectionVector.setMag(segmentLength);
         endPoint.set(newEndpoint);
+        //lägg till kontroll på begränsningsvinklar
+        //om bägränsning är passerad sätt vinkeln till begränsning
+        //och beräkna om ny startpoint för vektorn för att hålla gripklon "bakom" önskad enpoint.
         return PVector.sub(newEndpoint, newDirectionVector, startPoint);
     }
 
@@ -76,12 +111,44 @@ public class ArmSegment {
         //returnerar slutpositionen vektorn
         PVector diff = PVector.sub(newStartPoint, startPoint);
         startPoint.set(newStartPoint);
+        System.out.println("forward start: " + startPoint + endPoint);
+
         if(parent != null){
-             relativeAngle = parent.absoluteAngle - absoluteAngle;
+             relativeAngle = parent.absoluteAngle + absoluteAngle;
         } else {
             relativeAngle = absoluteAngle;
         }
-        return PVector.add(endPoint,diff, endPoint);
+
+        System.out.println("relative: " + Math.toDegrees(relativeAngle) + " constraint UP: " + Math.toDegrees(constraintXtoYUp) + " constraint down: " + Math.toDegrees(constraintXtoYDown));
+        if(relativeAngle > constraintXtoYUp){
+            System.out.println("begränsning uppåt");
+            relativeAngle = constraintXtoYUp;
+            setEndPointByConstraint();
+        } else if (relativeAngle < constraintXtoYDown) {
+            System.out.println("begränsning nedåt");
+            relativeAngle = constraintXtoYDown;
+            setEndPointByConstraint();
+        } else {
+            //within constraint
+//            return PVector.add(endPoint,diff, endPoint);
+            PVector.add(endPoint,diff, endPoint);
+        }
+        System.out.println("forward end: " + startPoint + endPoint + "\n");
+        return endPoint;
+    }
+
+    private void setEndPointByConstraint() {
+        System.out.println("angles before relative: " + Math.toDegrees(relativeAngle) + " | absolute: " + Math.toDegrees(absoluteAngle));
+        if(parent!=null){
+            absoluteAngle = relativeAngle - parent.absoluteAngle;
+        } else {
+            absoluteAngle = relativeAngle;
+        }
+        System.out.println("angles after relative: " + Math.toDegrees(relativeAngle) + " | absolute: " + Math.toDegrees(absoluteAngle));
+        PVector newEndpointDirectionVector = PVector.fromAngle(absoluteAngle);
+        newEndpointDirectionVector.setMag(segmentLength);
+        PVector.add(startPoint,newEndpointDirectionVector,endPoint);
+        System.out.println("new vectors in constraints: " + startPoint + endPoint + newEndpointDirectionVector);
     }
 
     protected Float calculateDiffToTarget(PVector newEndpoint) {
