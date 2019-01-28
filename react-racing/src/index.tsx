5256590b32beadf6ad9fbc1afda84acdf448186d
Joel Binnquist <joel.binnquist@gmail.com>
2019-01-28 20:38:22 +0100

    Store i context

--- react-racing/src/index.tsx
+++ react-racing/src/index.tsx
@@ -5,25 +5,35 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import {
+  createStore,
+  Store
+} from 'redux';
 import { BackendEventChannel } from './backend-event-channel/backend-event-channel';
 import { composeWithDevTools, } from 'redux-devtools-extension';
-import { rootReducer } from './state';
+import {
+  rootReducer,
+  RootState
+} from './state';
 import { ClientApi } from './backend-event-channel/client-api';
-import {changeLoginStatus} from './App.state';
+import {
+  changeLoginStatus
+} from './App.state';
 
-const store = createStore(
+const store: Store<RootState> = createStore(
   rootReducer,
   composeWithDevTools()
 );
 
 export interface IAppContext {
   clientApi: ClientApi;
+  store: Store<RootState>
 }
 export const AppContext = React.createContext<IAppContext>({} as IAppContext);
 export const AppContextConsumer = AppContext.Consumer;
 const appContext: IAppContext = {
-  clientApi: new ClientApi(process.env.REACT_APP_CLIENT_API_BASE_URL)
+  clientApi: new ClientApi(process.env.REACT_APP_CLIENT_API_BASE_URL),
+  store
 };
 
 appContext.clientApi.userForToken()
